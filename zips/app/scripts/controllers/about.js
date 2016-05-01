4facb787bc66d9de546275d5bae8d1eae392d3df
Daniel Lim <hbt@markhamboards.com>
2016-04-30 22:16:47 -0400

    add new yo

---
+++ zips/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zipsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zipsApp
+ */
+angular.module('zipsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ zips/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zipsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zipsApp
+ */
+angular.module('zipsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
