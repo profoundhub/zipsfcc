8930d609c55b531e5326c6bb7981b3d44811fc82
Daniel Lim <hbt@markhamboards.com>
2016-08-15 13:02:12 -0400

    updates
    updates

--- zips/app/api/models/ziplines.model.js
+++ zips/app/api/models/ziplines.model.js
@@ -1,16 +1,24 @@
 'use strict';
 
-//https://docs.mongodb.org/manual/reference/connection-string/
-//https://www.mongodb.com/blog/post/building-your-first-application-mongodb-creating-rest-api-using-mean-stack-part-1
-//http://stackoverflow.com/questions/21715590/sort-an-nested-array-in-mongoose
-//https://docs.mongodb.org/getting-started/node/query/
-//http://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+// https://docs.mongodb.org/manual/reference/connection-string/
+// https://www.mongodb.com/blog/post/building-your-first-application-mongodb-creating-rest-api-using-mean-stack-part-1
+// http://stackoverflow.com/questions/21715590/sort-an-nested-array-in-mongoose
+// https://docs.mongodb.org/getting-started/node/query/
+// http://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+    db.on('error', console.error);
+    db.once('open', function() {
+      // Create your schemas and models here.
+
+    });
+
 var TagsSchema = new Schema({
-  tag: String  
+  tag: String
 });
+
 
 TagsSchema.statics.findByName = function findByName(value, cb){
   //return this.model('Tags').find({tag:value}, cb);
@@ -45,34 +53,30 @@ var ZiplinesSchema = new Schema({
 });
 
 ZiplinesSchema.statics.findByUri = function findByUri(value, cb){
-  //return this.model('Ziplines').find({uri:value}, cb);
+  return this.model('Ziplines').find({uri:value}, cb);
   return this.find({uri:value}, cb);
 }
 
 module.exports.Ziplines = mongoose.model('Ziplines', ZiplinesSchema);
 
+var ziplineSchema = new Schema({
+ _owner: {type:mongoose.Schema.Types.ObjectId, ref:'Ziplines'},
+ title: String,
+ url: String,
+ created: {type: Date},
+ modified:{type: Date},
+ //tags: {type:[], get: getTags, set: setTags},
+ tags: [],
+ views: Number,
+ comments: Number,
+ hearts: Number,
+ info: String,
+ active: Boolean
+});
 
+var ZiplinesSchema = new Schema({
+ owner: {type: String, required: true},
+ ziplines: [ziplineSchema]
+});
 
-
-
-//var ziplineSchema = new Schema({
-//  _owner: {type:mongoose.Schema.Types.ObjectId, ref:'Ziplines'},
-//  title: String,
-//  url: String,
-//  created: {type: Date},
-//  modified:{type: Date},
-//  //tags: {type:[], get: getTags, set: setTags},
-//  tags: [],
-//  views: Number,
-//  comments: Number,
-//  hearts: Number,
-//  info: String,
-//  active: Boolean
-//});
-//
-//var ZiplinesSchema = new Schema({
-//  owner: {type: String, required: true},
-//  ziplines: [ziplineSchema]
-//});
-//
-//module.exports = mongoose.model('Ziplines', ZiplinesSchema);
+module.exports = mongoose.model('Ziplines', ZiplinesSchema);
